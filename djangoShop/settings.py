f4987adb53384c814e485d9d78c2b7fd28c5fa72
alvinist27 <suhrobhotamov496@gmail.com>
2022-12-25 23:46:50 +0300

    Added poetry, fixed flake8 and mypy warnings

--- djangoShop/settings.py
+++ djangoShop/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
 from pathlib import Path
+from typing import List
+
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,8 +24,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-from dotenv import load_dotenv
-
 load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
@@ -30,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS: List = []
 
 
 # Application definition
